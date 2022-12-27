5267f9a21740b1130886448ea1c36d95ac6d2e51
polyglot-vikash <viku.nitdgpe@gmail.com>
2022-12-28 01:08:42 +0530

    Initial commit

---
+++ handler.go
@@ -0,0 +1,16 @@
+// handler.go
+package main
+
+import "net/http"
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	// Handle index route
+}
+
+func usersHandler(w http.ResponseWriter, r *http.Request) {
+	// Handle users route
+}
+
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	// Handle user route
+}
